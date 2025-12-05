176c9e5c30826ac79da029a0f67edc9bc6658b2b
João Faganello <jfaganello2@gmail.com>
2025-12-05 16:03:29 -0300

    Preparado para o render?9

--- blog_jf/settings.py
+++ blog_jf/settings.py
@@ -121,7 +121,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage' 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")           
+STATIC_ROOT = BASE_DIR / "staticfiles" 
+STATICFILES_DIRS = [
+    BASE_DIR / "blog_jf" / "blog" / "static",
+]        
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
