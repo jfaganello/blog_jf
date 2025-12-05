35bee54f2c2293bb13a4889728befc0d91ad8823
João Faganello <jfaganello2@gmail.com>
2025-12-05 15:41:57 -0300

    Preparado para o render?7

--- blog_jf/settings.py
+++ blog_jf/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["SECRET_KEY"]
+SECRET_KEY = 'django-insecure-v^fob_yzl_5x@+h!#mm9j@#tzvqu5u3fi#&a-acxm^@djxxzz4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
