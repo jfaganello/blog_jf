78ea14eb3beb1a56ac9c0386065b39c1f2d0accd
João Faganello <jfaganello2@gmail.com>
2025-12-04 23:42:08 -0300

    primeiro commit, antes das views

---
+++ blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model):
+    titulo = models.CharField(max_length=200)
+    conteudo = models.TextField()
+    criado_em = models.DateTimeField(default=timezone.now)
+    atualizado_em = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.titulo
+
+    def get_absolute_url(self):
+        from django.urls import reverse
+        return reverse('post_detail', kwargs={'pk': self.pk})
