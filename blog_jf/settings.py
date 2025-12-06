830beca9117e0f6b73da9be3d9a7e48a36fc34fd
João Faganello <jfaganello2@gmail.com>
2025-12-05 21:16:04 -0300

    Preparado para o render?10

--- blog_jf/settings.py
+++ blog_jf/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-v^fob_yzl_5x@+h!#mm9j@#tzvqu5u3fi#&a-acxm^@djxxzz4
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", ".onrender.com"]
 
